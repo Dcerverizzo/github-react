c40c553b590d5c96fbef932593ff12dde7a887b2
Daniel Cerverizzo <daniel_cerverizo@hotmail.com>
2022-10-14 18:43:40 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Profile from './components/Profile';
+import Projects from './components/Projects';
+
+function App() {
+  return (
+    <div className="App">
+      <body>
+        <div>
+        <Profile/>
+        <Projects/>
+        </div>
+      </body>
+    </div>
+  );
+}
+
+export default App;
